3dd753b91313322edc7561b261d0fef64f5aa37e
Jack Liu <gliu1586@aspac.convergys.com>
2020-10-26 18:03:05 +0800

    Create the downloader to downloader the Avaya learning course.

--- DeleteHTML.R
+++ DeleteHTML.R
@@ -24,6 +24,38 @@ x <- digest("Hello", algo="md5")
 x
 
 
+print(getwd())
+
+download.file(url="https://www.avaya-learning.com/scormengine/ScormContentRoot/20930W_130520/content/mod06/topic04/page09.mp4",
+              destfile = "page09.mp4",
+              mode = "wb")
+
+print("Download completed \n")
+
+
+
+
+
+# download file in remote host
+downloader <- function(remotefile, localfile, mode){
+  cat("download start...", "\n")
+  
+  download.file(url = remotefile, 
+                destfile=localfile, 
+                mode = mode)
+  
+  cat("download End...", "\n")
+}
+
+
+remotefile <- "https://www.avaya-learning.com/scormengine/ScormContentRoot/20930W_130520/coursedata.json"
+localfile <- "20930W POM.json"
+
+downloader(remotefile, localfile, mode = "w")
+
+
+
+
 
 
 
